0d0f88386d914193681b84944baab0c0896f82b7
tsekhmeistruk <autocrossauto@gmail.com>
2015-08-30 18:40:52 +0300

    - add necessary class ()

---
+++ QvcTesterTool/ViewModel/DevicesViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JustForTestConsole;
+using JustForTestConsole.Helpers;
+using JustForTestConsole.Interfaces;
+using System.Management;
+
+namespace QvcTesterTool.ViewModel
+{
+    public class DevicesViewModel
+    {
+        private IAdbShell _adb;
+        public ObservableCollection<Device> Devices { get; set; }
+
+        public DevicesViewModel()
+        {
+            _adb = new AdbShell();
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            Devices = new ObservableCollection<Device>();
+            foreach (var item in _adb.GetDevices())
+            {
+                Devices.Add(new Device(item));
+            }
+        }
+    }
+}
